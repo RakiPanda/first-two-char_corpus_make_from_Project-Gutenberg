ee180b802a0013b5a3f9696e1645a0bff62216ab
RakiPanda <scrumhalf0415@gmail.com>
2024-05-28 15:38:11 +0900

    改行前半角スペースなくし，一文字単語の後ろに半角スペース

--- test_data_make_from_ProjectGutenberg_only_final_chapter.py
+++ test_data_make_from_ProjectGutenberg_only_final_chapter.py
@@ -16,8 +16,14 @@ sentences = re.split(r'(?<=[.!?])\s+', last_chapter_text)
 concatenated_result = []
 for sentence in sentences:
     words = re.findall(r'\b\w+\b', sentence)
-    # 各単語の最初の2文字を連結し、最後に半角スペースを追加する
-    concatenated_result.append(''.join([word[:2] for word in words]) + " ")
+    # 各単語の最初の2文字を連結し、単語が1文字の場合は後ろにスペースを追加する
+    processed_words = []
+    for word in words:
+        if len(word) == 1:
+            processed_words.append(word[:2] + " ")  # 1文字単語の後にスペースを追加
+        else:
+            processed_words.append(word[:2])
+    concatenated_result.append(''.join(processed_words))
 
 # 結果をテキストファイルに保存
 output_path = 'test_data_only_final_chapter.txt'
